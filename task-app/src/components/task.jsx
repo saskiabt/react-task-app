8be8125b8e2475a66fcc5b4c3c8ca19b55976d2b
saskiabt <sskbinder@gmail.com>
2022-07-28 19:58:13 -0400

    styling updates

--- task-app/src/components/task.jsx
+++ task-app/src/components/task.jsx
@@ -2,19 +2,21 @@ import React from "react";
 
 let completedStyling = {
     div: {
-        backgroundColor: "#4caf50",
+        backgroundColor: "rgb(172 219 174)",
         color: "white"
     }
 }
+
 function Task(props) {
     const {task,i,deleteTask,completeTask} = props; 
-    const renderCompletedTaskFormat = () => {
+    const conditionalTaskFormatting = () => {
         if (task.isCompleted) {
             return  <div id={i} key={i} className="list-item" style={completedStyling.div}>
                         <li>{task.text}</li>
                         <div className="button-box">
-                            <button className="complete-button" onClick={() =>completeTask(i)} style={{display:"none"}}>✓</button>
-                            <button className="delete-button" onClick={() =>deleteTask(i)}>🗑️ </button>
+                            <button className="complete" onClick={() =>completeTask(i)} style={{display:"none"}}>✓</button>
+                            <button className="delete" onClick={() =>deleteTask(i)}>🗑️ </button>
+                            <button className="edit">✎</button>
                         </div>
                        
                     </div>
@@ -22,15 +24,17 @@ function Task(props) {
                     <div id={i} key={i} className="list-item">
                         <li>{task.text}</li>
                         <div className="button-box">
-                            <button className="complete-button" onClick={() =>completeTask(i)}>✓</button>
-                            <button className="delete-button" onClick={() =>deleteTask(i)}>🗑️ </button>
+                            <button className="complete" onClick={() =>completeTask(i)}>✓</button>
+                            <button className="delete" onClick={() =>deleteTask(i)}>🗑️ </button>
+                            <button className="edit">✎</button>
+
                         </div>
                     </div>
         );
     };
 
     return (
-        renderCompletedTaskFormat()
+        conditionalTaskFormatting()
     )
 }
 
